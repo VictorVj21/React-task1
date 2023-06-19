46a861bce79067bb93089991c8cfd7879ab34765
VictorVj21 <130632816+VictorVj21@users.noreply.github.com>
2023-06-19 14:40:02 +0530

    Bootstrap pricecart

---
+++ App.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <section>
+        <h3>FREE</h3>
+        <h4>$0/month</h4>
+        <p>______________</p>
+        <div>
+          <p>✔ Single User</p>
+          <p>✔ 5GB Storage</p>
+          <p>✔ Unlimited Public Projects</p>
+          <p>✔ Community Access</p>
+          <p>X Unlimited Private Projects</p>
+          <p>X Dedicated Phone Support</p>
+          <p>X Free Subdomain </p>
+          <p>X Monthly Status Report</p>
+            <button>BUTTON</button>
+          </div>
+          </section>
+      </div>
+
+      <div>
+        <h3>PLUS</h3>
+        <h4>$9/month</h4>
+        <p>______________</p>
+        <div>
+          <p>✔ <b>5 Users</b></p>
+          <p>✔ 50GB Storage</p>
+          <p>✔ Unlimited Public Projects</p>
+          <p>✔ Community Access</p>
+          <p>✔ Unimited Private Projects</p>
+          <p>✔ Dedicated Phone Support</p>
+          <p>✔ Free Subdomain </p>
+          <p>X Monthly Status Report</p>
+          <button>BUTTON</button>
+        </div>
+      </div>
+
+      <div>
+        <h3>PRO</h3>
+        <h4>$49/month</h4>
+        <p>______________</p>
+        <div>
+          <p>✔ <b>Unlimited Users</b></p>
+          <p>✔ 15G0B Storage</p>
+          <p>✔ Unlimited Public Projects</p>
+          <p>✔ Community Access</p>
+          <p>✔ Unlimited Private Projects</p>
+          <p>✔ Dedicated Phone Support</p>
+          <p>✔ <b>Unlimited</b> Free Subdomain </p>
+          <p>✔ Monthly Status Report</p>
+          <button>BUTTON</button>
+        </div>
+
+      </div>
+
+
+
+    </div>
+  )
+}
+
+export default App
